f4ca84dfb3a51e15eabe84593b046d9adf1feef9
adityav2410 <adityav2410@gmail.com>
2018-02-28 22:43:34 -0800

    Re-architect library - support for inheritance

--- HeapInPython/README.md
+++ HeapInPython/README.md
@@ -1,31 +1,34 @@
 ## A toy library in Python for MinHeap and MaxHeap
 
-### MaxHeap
-```
-vals = [2,4,9, 1, 4, 54,5]
-heap = MaxHeap(vals)
-heap.push(27)
-heap.pop()
-for x in heap:
-    print x,
 
+
+
+### Heap 
 ```
+from heap import MinHeap, MaxHeap
+
+vals = np.random.randint(25,size=10)
+maxHeap = MaxHeap(vals)
+minHeap = MinHeap(vals)
+maxHeap.push(19)
+minHeap.push(29)
+
+print "MinHeap"
+for x in minHeap:
+    print x,
+print "\n\n\nMaxHeap"
+for x in maxHeap:
+    print x,
+```
+
+
 #### Output
 ```
-27 9 5 4 4 2 1
-```
+MinHeap
+2 6 6 6 12 13 13 18 23 24 29 
 
-### MinHeap
-```
-vals = [2,4,9, 1, 4, 54,5]
-heap = MinHeap(vals)
-heap.push(27)
-heap.pop()
-for x in heap:
-    print x,
 
-```
-#### Output
-```
-2 4 4 5 9 27 54
+MaxHeap
+24 23 19 18 13 13 12 6 6 6 2
+
 ```
--- HeapInPython/heap.py
+++ HeapInPython/heap.py
@@ -1,38 +1,8 @@
 import heapq
 
-class MaxHeap(object):
-    
-    def __init__(self, vals=[]):
+class Heap(object):
+    def __init__(self):
         self._heap = []
-        self._heap[:] = [-val for val in vals ]
-        heapq.heapify(self._heap)
-        
-    def push(self, val):
-        heapq.heappush(self._heap, -val)
-    
-    def pop(self):
-        item = -heapq.heappop(self._heap)
-        return item
-        
-    def size(self):
-        return len(self._heap)
-    
-    def __iter__(self):
-        return self
-    
-    def next(self):
-        if len(self._heap):
-            return self.pop()
-        else:
-            raise StopIteration
-            
-            
-class MinHeap(object):
-    
-    def __init__(self, vals=[]):
-        self._heap = []
-        self._heap[:] = [val for val in vals ]
-        heapq.heapify(self._heap)
         
     def push(self, val):
         heapq.heappush(self._heap, val)
@@ -41,6 +11,15 @@ class MinHeap(object):
         item = heapq.heappop(self._heap)
         return item
         
+    def empty(self):
+        return self.size() == 0
+
+    def top(self):
+        if self.empty():
+            raise IndexError
+        else:
+            return self._heap[0]
+
     def size(self):
         return len(self._heap)
     
@@ -52,3 +31,32 @@ class MinHeap(object):
             return self.pop()
         else:
             raise StopIteration
+            
+    def heapify(self):
+        heapq.heapify(self._heap)
+
+
+class MaxHeap(Heap):
+    def __init__(self, vals=[]):
+        super(MaxHeap,self).__init__()
+        self._heap[:] = [-val for val in vals ]
+        self.heapify()
+        
+    def push(self, val):
+        super(MaxHeap,self).push(-val)
+    
+    def pop(self):
+        return -super(MaxHeap,self).pop()
+
+class MinHeap(Heap):
+    def __init__(self, vals=[]):
+        super(MinHeap,self).__init__()
+        self._heap[:] = [val for val in vals ]
+        self.heapify()
+        
+    def push(self, val):
+        super(MinHeap,self).push(val)
+    
+    def pop(self):
+        return super(MinHeap, self).pop()
+        
